35d0ef252e80c638dfd69c139d72cacbeca2cf83
anikait-reddy15 <anikait727@gmail.com>
2025-03-23 01:37:30 +0530

    kaggle api creation 2

--- main.py
+++ main.py
@@ -5,16 +5,13 @@ import subprocess
 st.set_page_config(page_title="AI Dataset generator", page_icon=":1234:")
 st.header("Dataset Generator")
 
-options = ("Health", "Finance", "E-commerce", "Social media", "Environmental science",
-           "Education", "Transportation", "Marketing", "Agriculture", "Energy", "Cybersecurity",
-           "Governement", "Real estate", "Entertainment")
 
-selected_option = st.selectbox(f"Choose an option : ", options)
-text_input = st.text_input("Explain about the dataset in detail : ")
+text_input = st.text_input("Dataset required : ")  
 
 if st.button("Submit"):
     if text_input:
         try:
+            cleaned_input = text_input.strip().lower()
             command = ["kaggle", "datasets", "list", "-s", text_input, "--json"]
             process = subprocess.run(command, capture_output=True, text=True, check=True)
             datasets = json.loads(process.stdout)
